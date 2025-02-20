d59490a4d81e9e7a9e49fbb4f0426bbef8686b2b
Lakdinu Jayawardhana <lakdinurameshjayawardhana@gmail.com>
2025-02-20 17:27:58 +0530

    Add Print class with main method for basic output

---
+++ exercises/print.java
@@ -0,0 +1,7 @@
+public class Print {
+    public static void main(String[] args) {
+        System.out.println("Hi");
+        System.out.println("I'm lakdinu");
+        System.out.println("I'm learning Java");
+    }
+}
