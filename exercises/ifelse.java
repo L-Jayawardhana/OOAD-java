32bb8e1ae72f84a44e33b9b21db52c234e3f9105
Lakdinu Jayawardhana <lakdinurameshjayawardhana@gmail.com>
2025-02-21 18:36:10 +0530

    choose cetagory using age

--- exercises/ifelse.java
+++ exercises/ifelse.java
@@ -1,5 +1,12 @@
 public class ifelse {
     public static void main(String[] args){
-        System.out.println("hello");
+        int age = -9;
+         if (age <= 0){
+             System.out.println("You have been not born yet! 😒");
+         }else if (age < 18){
+             System.out.println("You area a teenager 😊");
+         }else{
+             System.out.println("You are an adult 😁");
+         }
     }
 }
