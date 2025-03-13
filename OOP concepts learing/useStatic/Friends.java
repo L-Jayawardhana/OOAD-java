7a3fe95145bbed481d330c9690a23788d8557730
Lakdinu Jayawardhana <lakdinurameshjayawardhana@gmail.com>
2025-03-13 14:06:21 +0530

    Class Friends for static example

--- OOP concepts learing/useStatic/Friends.java
+++ OOP concepts learing/useStatic/Friends.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
-
 public class Friends {
+    static int noOfFriends;
+    String name;
+
+    Friends(String name){
+        this.name = name;
+        noOfFriends++;
+    }
+
+    //since noOfFriends is static we don't have to call using this.noOfFriends
+    //we can directly call using variable name
+    void friendsCount(){
+        System.out.println("you have " + noOfFriends + " friends");
+    }
 }
