3773f799885f9509104af11a716a626c2533e88c
L-Jayawardhana <lakdinurameshjayawardhana@gmail.com>
2025-06-13 17:22:02 +0530

    Update Main20.java

--- OOP concepts learning/FileReading/Main20.java
+++ OOP concepts learning/FileReading/Main20.java
@@ -1,3 +1,6 @@
+import jdk.internal.access.JavaIOFileDescriptorAccess;
+import jdk.internal.jmod.JmodFile;
+
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -12,16 +15,29 @@ public class Main20 {
         // RandomAccessFile          : Best for read/write specific portions of a large file.
 
         String filePath = "testread.txt";
+        BufferedReader reader = null;
 
-        try(BufferedReader reader = new BufferedReader(new FileReader(filePath))){
+        try {
+            reader = new BufferedReader(new FileReader(filePath));
             String line;
-            while((line = reader.readLine()) != null){
+            while ((line = reader.readLine()) != null) {
                 System.out.println(line);
             }
         } catch (FileNotFoundException e) {
             System.out.println("file can't be located");
         } catch (IOException e) {
             System.out.println("Something went wrong");
+        }
+        finally {
+            // This block is optional, but can be used for cleanup actions
+           if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    System.out.println("Error closing the reader");
+                }
+            }
+            System.out.println("File reading completed.");
         }
     }
 }
